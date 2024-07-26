7750951a080dff19616051b8390aa4c8a5c8ed19
Hanglesson <hanglesson.dev@gmail.com>
2024-07-26 16:33:01 -0500

    chore: :hammer: simple supabase test integration

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="flex h-screen items-center justify-center bg-gray-900 p-5">
-    <InputText placeholder="Overriden" />
-    <Button icon="pi pi-check" label="Click" />
+  <div class="flex h-screen bg-gray-900">
+    <NuxtPage />
   </div>
 </template>
