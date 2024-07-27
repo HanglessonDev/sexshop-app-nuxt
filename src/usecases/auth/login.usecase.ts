db3a593316fb25a7ce2f9fb59dcd68c88983e1af
Hanglesson <hanglesson.dev@gmail.com>
2024-07-27 04:56:30 -0500

    refactor: 🔄 refactored Auth composable

--- src/usecases/auth/login.usecase.ts
+++ src/usecases/auth/login.usecase.ts
@@ -1,4 +1,4 @@
-import type { LoginDTO, SessinDTO } from '~/src/domain/dto/auth.dto'
+import type { LoginDTO, SessionDTO } from '~/src/domain/dto/auth.dto'
 import type { AuthGateway } from '~/src/domain/gateways/auth.gateway'
 
 export class LoginUseCase {
@@ -8,7 +8,7 @@ export class LoginUseCase {
     return new LoginUseCase(authGateway)
   }
 
-  public async execute(data: LoginDTO): Promise<SessinDTO | null> {
+  public async execute(data: LoginDTO): Promise<SessionDTO | null> {
     return await this.authGateway.login(data)
   }
 }
